bc9f2dbb20a1d917465ad5d19dcce7e52bae8e38
Lwandile Rozani <vsemoth@gmail.com>
2021-03-14 14:14:35 +0200

    Second Commit

--- server.js
+++ server.js
@@ -1,118 +1,23 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
 const express = require('express')
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const jwt = require('jsonwebtoken')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
+const expressLayout =  require('express-ejs-layouts')
 
 const app = express()
+const port = 5000
 
-const initializePassport = require('./passport-config')
+// Set Templating Engine
+app.use(expressLayout)
+app.use('view engine', 'ejs')
 
-initializePassport(
-  passport, 
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
-
-const users = []
-
-app.set('view-engine', 'ejs')
-
-app.use(express.urlencoded({extended: false}))
-
-app.use(flash())
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-
+// Static File
+app.use(app.router);
+routes.initialize(app);
 app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
 
-app.use('/css', express.static(__dirname + '/css'))
-
-app.use(express.json())
-
-const posts = [
-  {
-    username: 'Lwandile',
-    title: 'Post 1'
-  },
-  {
-    username: 'Kyle',
-    title: 'Post 2'
-  }
-]
-
-app.get('/posts', (req, res) => {
-  res.json(posts)
+// Navigation
+app.get('', (req, res) => {
+  res.render('index.ejs')
 })
 
-app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', {name: req.user.name})
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs')
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}), (req, res) => {
-  /*const username = req.body.username
-  const user = {name: req.body.name}*/
-  accessToken => jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json({accessToken: accessToken})
-})
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs')
-})
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect('/login')
-  } catch {
-    res.redirect('/register')
-  }
-})
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  
-  res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/')
-  }
-  next()
-}
-
-app.listen(5000)
+// Listen on Port 5000
+app.listen(port, () => console.info(`App listening on port ${port}`))
