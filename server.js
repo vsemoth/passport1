f7dbd15527589956d4d007a0039747957432a0f9
Lwandile Rozani <vsemoth@gmail.com>
2021-03-14 19:14:03 +0200

    Included Node Modules

--- server.js
+++ server.js
@@ -1,23 +1,123 @@
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
-const expressLayout =  require('express-ejs-layouts')
+
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const jsdom = require("jsdom");
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
 
 const app = express()
-const port = 5000
 
-// Set Templating Engine
-app.use(expressLayout)
-app.use('view engine', 'ejs')
+const initializePassport = require('./passport-config')
 
-// Static File
-app.use(app.router);
-routes.initialize(app);
+initializePassport(
+  passport, 
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
 
-// Navigation
-app.get('', (req, res) => {
-  res.render('index.ejs')
+app.use('/css', express.static(__dirname + '/css'))
+
+app.use(express.json())
+
+const posts = [
+  {
+    username: 'Lwandile',
+    title: 'Post 1'
+  },
+  {
+    username: 'Kyle',
+    title: 'Post 2'
+  }
+]
+
+app.get('/posts', (req, res) => {
+  res.json(posts)
 })
 
-// Listen on Port 5000
-app.listen(port, () => console.info(`App listening on port ${port}`))
+app.get('/gallery', checkAuthenticated, (req, res) => {
+  res.render('gallery.ejs', {name: req.user.name})
+})
+
+app.get('/', checkNotAuthenticated, (req, res) => {
+  res.render('layout.ejs')
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(5000, () => console.log("App running on port 5000"))
